4835eaa716f8d1d9b0cc0ccd3646b5c8ac56486b
Tercio Lemos <tercio.lemos@gmail.com>
2023-02-27 22:29:49 -0300

    environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   company: "CTA Cargo Prod",
   ErrorTimeout: 7000,
-  BaseURL: "https://dev-wa-cctimportacao.azurewebsites.net/api/",
-  BaseUrlApiCta: "https://dev-wa-cctimportacao.azurewebsites.net",
+  BaseURL: "https://cta-webapi-prod.azurewebsites.net/api/",
+  BaseUrlApiCta: "https://cta-webapi-prod.azurewebsites.net",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,4 @@ export const environment = {
   ErrorTimeout: 7000,
   BaseURL: "https://cta-webapi-prod.azurewebsites.net/api/",
   BaseUrlApiCta: "https://cta-webapi-prod.azurewebsites.net",
-  //BaseURL: "https://localhost:5001/api/",
-  //BaseUrlApiCta: "https://localhost:5001",
 };

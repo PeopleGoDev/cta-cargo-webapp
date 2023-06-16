c226c3bdad2c52fecde7cb6b3952b2f3d55ab6ed
Tercio Lemos <tercio.lemos@gmail.com>
2023-06-16 15:20:14 -0300

    Ultimos ajustes pc GBI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   company: "CTA Cargo",
   ErrorTimeout: 7000,
-  // BaseURL: "https://localhost:5001/api/",
-  // BaseUrlApiCta: "https://localhost:5001",
-  BaseURL: "https://cta-webapi-prod.azurewebsites.net/api/",
-  BaseUrlApiCta: "https://cta-webapi-prod.azurewebsites.net",
+  BaseURL: "https://localhost:5001/api/",
+  BaseUrlApiCta: "https://localhost:5001",
+  // BaseURL: "https://cta-webapi-prod.azurewebsites.net/api/",
+  // BaseUrlApiCta: "https://cta-webapi-prod.azurewebsites.net",
 };
